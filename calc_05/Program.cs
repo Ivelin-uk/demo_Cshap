1547a1531d1135038e3259e6cf32e0b9bcdac90b
Ivelin-uk <ivoagaininwow@gmail.com>
2024-11-23 23:05:31 +0200

    first commit

---
+++ calc_05/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter number one: ");
+int numberOne = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number two: ");
+int numberTwo = int.Parse(Console.ReadLine());
+int sum = numberOne + numberTwo;
+
+Console.WriteLine("The sum is: ");
+Console.WriteLine(sum);
